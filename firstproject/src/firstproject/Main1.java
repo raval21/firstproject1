cb3457a0079477b47e94ada712f91be5fddd1052
Raj <Raj@DESKTOP-J7QQ2UP.mshome.net>
2023-04-05 23:28:22 -0700

    update 1

---
+++ firstproject/src/firstproject/Main1.java
@@ -0,0 +1,8 @@
+package firstproject;
+
+public class Main1 {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ firstproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Raj
+ *
+ */
+module firstproject {
+}
